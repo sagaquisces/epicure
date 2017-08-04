ce1975c0f0197af8eb683617559b2ba0f550be15
Michael Dunlap <sagaquisces@gmail.com>
2017-08-04 14:21:33 -0700

    page will show last names in a panel header via javascript/jquery dom creation.

--- js/doctors-by-condition-interface.js
+++ js/doctors-by-condition-interface.js
@@ -4,8 +4,27 @@ var displayDoctorsByCondition = function(medicalIssue, doctorData) {
   $('#showDoctorsByConditionHeader').empty();
   $('#showDoctorsByConditionUl').empty();
   $('#showDoctorsByConditionHeader').append(medicalIssue);
+  var panelTitleA = "";
+  var i = 0;
+  var $target;
   doctorData.forEach(function(doctor) {
-    $('#showDoctorsByConditionUl').append('<li class="list-group-item">' + doctor.profile.first_name + " " + doctor.profile.last_name + '</li>');
+    // panelTitleString = '<a data-toggle="collapse" href="#collapse' + i + '">' + doctor.profile.first_name + ' '  + doctor.profile.last_name + '</a>';
+    panel = document.createElement('div');
+    panel.setAttribute('class', 'panel panel-default');
+    panelHead = document.createElement('div');
+    panelHead.setAttribute('class', 'panel-heading');
+    panelTitle = document.createElement('h4');
+    panelTitle.setAttribute('class', 'panel-title');
+    panelTitle.append(doctor.profile.last_name);
+    panelHead.appendChild(panelTitle);
+    panel.appendChild(panelHead);
+    $('#doctors').append(panel);
+
+    // panelTitleString += doctor.profile.last_name;
+    // panelTitleString +=
+
+    i++
+    // $('#showDoctorsByConditionUl').append('<li class="list-group-item">' + doctor.profile.first_name + " " + doctor.profile.last_name + '</li>');
   });
   // doctorData.forEach(function(doctor) {
   //   console.log(doctor.profile.first_name);
