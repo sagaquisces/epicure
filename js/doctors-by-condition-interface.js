ac166534c81fe6ee579f489ec83522ace68b34a8
Michael Dunlap <sagaquisces@gmail.com>
2017-08-04 16:06:38 -0700

    styled bio and picture with bootstrap media objects

--- js/doctors-by-condition-interface.js
+++ js/doctors-by-condition-interface.js
@@ -2,39 +2,73 @@ var SearchDoctors = require('./../js/searchDoctors.js').searchDoctorsModule;
 
 var displayDoctorsByCondition = function(medicalIssue, doctorData) {
   $('#showDoctorsByConditionHeader').empty();
-  $('#showDoctorsByConditionUl').empty();
+  $('#doctors').empty();
   $('#showDoctorsByConditionHeader').append(medicalIssue);
-  var panel, panelHead, panelTitle, panelA;
-  var i = 0;
-  doctorData.forEach(function(doctor) {
-    panel = document.createElement('div');
-    panel.setAttribute('class', 'panel panel-default');
 
-    panelHead = document.createElement('div');
-    panelHead.setAttribute('class', 'panel-heading');
-    panelTitle = document.createElement('h4');
-    panelTitle.setAttribute('class', 'panel-title');
-    panelA = document.createElement('a');
-    panelA.setAttribute('data-toggle', 'collapse');
-    panelA.setAttribute('href', '#collapse'+i);
-    panelA.append(doctor.profile.last_name);
-    panelTitle.appendChild(panelA);
-    panelHead.appendChild(panelTitle);
-    panel.appendChild(panelHead);
+  if(doctorData.length===0){
+    console.log ("this instance is empty");
+  } else {
+    var media, mediaLeft, mediaImageSmall, mediaBody, mediaP;
+    var panel, panelHead, panelTitle, panelA;
+    var panelCollapse, panelBody;
+    var i = 0;
+    doctorData.forEach(function(doctor) {
+      // first create Bootstrap media object
 
-    panelCollapse = document.createElement('div');
-    panelCollapse.setAttribute('id', 'collapse'+i);
-    panelCollapse.setAttribute('class', 'panel-collapse collapse');
-    panelBody = document.createElement('div');
-    panelBody.setAttribute('class', 'panel-body');
-    panelBody.append(doctor.profile.bio);
-    panelCollapse.appendChild(panelBody);
-    panel.appendChild(panelCollapse);
+      media = document.createElement('div');
+      media.setAttribute('class', 'media');
+      mediaLeft = document.createElement('div');
+      mediaLeft.setAttribute('class', 'media-left');
+      mediaImageSmall = document.createElement('img');
+      $(mediaImageSmall).attr({
+        src: doctor.profile.image_url,
+        title: "",
+        alt: doctor.profile.last_name,
+        height: "123px",
+        class: "media-object"
+      });
+      mediaBody = document.createElement('div');
+      mediaBody.setAttribute('class', 'media-body');
+      mediaP = document.createElement('p');
+      mediaP.append(doctor.profile.bio);
+      mediaBody.appendChild(mediaP);
+      mediaLeft.appendChild(mediaImageSmall);
+      media.appendChild(mediaLeft);
+      media.appendChild(mediaBody);
 
-    $('#doctors').append(panel);
+      //then the panel
 
-    i++
-  });
+      panel = document.createElement('div');
+      panel.setAttribute('class', 'panel panel-default');
+
+      panelHead = document.createElement('div');
+      panelHead.setAttribute('class', 'panel-heading');
+      panelTitle = document.createElement('h4');
+      panelTitle.setAttribute('class', 'panel-title');
+      panelA = document.createElement('a');
+      panelA.setAttribute('data-toggle', 'collapse');
+      panelA.setAttribute('href', '#collapse'+i);
+      panelA.append(doctor.profile.last_name);
+      panelTitle.appendChild(panelA);
+      panelHead.appendChild(panelTitle);
+      panel.appendChild(panelHead);
+
+      panelCollapse = document.createElement('div');
+      panelCollapse.setAttribute('id', 'collapse'+i);
+      panelCollapse.setAttribute('class', 'panel-collapse collapse');
+      panelBody = document.createElement('div');
+      panelBody.setAttribute('class', 'panel-body');
+
+      panelBody.append(media);
+      panelCollapse.appendChild(panelBody);
+      panel.appendChild(panelCollapse);
+
+      $('#doctors').append(panel);
+
+      i++;
+    });
+
+  }
 
 };
 
